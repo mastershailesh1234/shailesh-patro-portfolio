a0acc99e0ca38c3ab4d151f15a246ac8d83467ca
mastershailesh1234 <54624049+mastershailesh1234@users.noreply.github.com>
2021-06-28 08:54:12 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -21,7 +21,7 @@ import {
 import { projects } from "../../constants/constants";
 
 const Projects = () => (
-  <Section nopadding id="projects">
+  <Section id="projects">
     <SectionDivider />
     <SectionTitle main>Projects</SectionTitle>
     <GridContainer>
