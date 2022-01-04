e71cc34b84ee3a51709a7928defe7eacd0726406
Shailesh Patro <54624049+mastershailesh1234@users.noreply.github.com>
2022-01-04 07:57:39 +0530

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -40,7 +40,7 @@ const Footer = () => {
           <SocialIcons href="https://www.linkedin.com/in/shailesh-patro-4b28901b3/">
             <AiFillLinkedin size="3rem" />
           </SocialIcons>
-          <SocialIcons href="https://www.instagram.com/master_shailesh_1234/">
+          <SocialIcons href="https://www.instagram.com/_._.rimuru_tempest._._/">
             <AiFillInstagram size="3rem" />
           </SocialIcons>
           <SocialIcons href="https://codeforces.com/profile/master_coder1234">
